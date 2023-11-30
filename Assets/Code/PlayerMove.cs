52d381ffd85b50ed5b5067bb8d8008105e03516f
Sabrina-Tseng <tsens543@newschool.edu>
2023-11-30 17:35:18 -0500

    character can walk

--- Assets/Code/PlayerMove.cs
+++ Assets/Code/PlayerMove.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {    
     Rigidbody2D rb;
+    Animator anim;
 
     //game manager
     private GameManager gameManager;
 
     //move
-    public int speed = 5;
+    public float speed = 2.5f;
     public int jumpForce = 1000;
     public int jumpForceSmall = 200;
     public bool grounded;
@@ -31,15 +32,15 @@ public class PlayerMove : MonoBehaviour
     {    
         gameManager = FindObjectOfType<GameManager>();
         rb = GetComponent<Rigidbody2D>();
-        // anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         float xSpeed1 = Input.GetAxis("Horizontal1") * speed;
-        float xSpeedSlow1 = Input.GetAxis("Horizontal1") * speed * 0.2f;
+        float xSpeedSlow1 = Input.GetAxis("Horizontal1") * speed * 0.25f;
         float xSpeed2 = Input.GetAxis("Horizontal2") * speed;
-        float xSpeedSlow2 = Input.GetAxis("Horizontal2") * speed * 0.2f;
+        float xSpeedSlow2 = Input.GetAxis("Horizontal2") * speed * 0.25f;
         float xSpeed = 0;
 
         //movement
@@ -51,6 +52,14 @@ public class PlayerMove : MonoBehaviour
         // {
         //     rb.velocity = new Vector2(xSpeed, rb.velocity.y);
         // }
+
+        if (xSpeed == 0)
+        {
+            anim.SetBool("Walk", false);
+            anim.SetBool("DragR", false);
+            anim.SetBool("DragL", false);
+        }
+
         if(Input.GetAxis("Horizontal1") < 0) 
         {
             if(Input.GetAxis("Horizontal2") < 0)
@@ -58,6 +67,7 @@ public class PlayerMove : MonoBehaviour
                 //same direction
                 rb.velocity = new Vector2(xSpeed1, rb.velocity.y);
                 xSpeed = xSpeed1;
+                anim.SetBool("Walk", true);
             }
             else if(Input.GetAxis("Horizontal2") > 0)
             {
@@ -68,6 +78,7 @@ public class PlayerMove : MonoBehaviour
                 //only player one pressed
                 rb.velocity = new Vector2(xSpeedSlow1, rb.velocity.y);
                 xSpeed = xSpeedSlow1;
+                anim.SetBool("DragL", true);
             }
         }
         if(Input.GetAxis("Horizontal1") > 0) 
@@ -76,6 +87,7 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeed1, rb.velocity.y);
                 xSpeed = xSpeed1;
+                anim.SetBool("Walk", true);
             }
             else if(Input.GetAxis("Horizontal2") < 0)
             {
@@ -85,6 +97,7 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeedSlow1, rb.velocity.y);
                 xSpeed = xSpeedSlow1;
+                anim.SetBool("DragR", true);
             }        
         }
         if(Input.GetAxis("Horizontal2") < 0) 
@@ -93,6 +106,7 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeed2, rb.velocity.y);
                 xSpeed = xSpeed2;
+                anim.SetBool("Walk", true);
             }
             else if(Input.GetAxis("Horizontal1") > 0)
             {
@@ -102,6 +116,7 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeedSlow2, rb.velocity.y);
                 xSpeed = xSpeedSlow2;
+                anim.SetBool("DragL", true);
             }
         }
         if(Input.GetAxis("Horizontal2") > 0) 
@@ -110,6 +125,7 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeed2, rb.velocity.y);
                 xSpeed = xSpeed2;
+                anim.SetBool("Walk", true);
             }
             else if(Input.GetAxis("Horizontal1") < 0)
             {
@@ -119,15 +135,16 @@ public class PlayerMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(xSpeedSlow2, rb.velocity.y);
                 xSpeed = xSpeedSlow2;
+                anim.SetBool("DragR", true);
             }        
         }
         
-        //direction
-        if ((xSpeed < 0 && transform.localScale.x > 0) || (xSpeed > 0 && transform.localScale.x < 0))
-        {
-            transform.localScale *= new Vector2(-1, 1);
-            dir *= -1;
-        }
+        // //direction
+        // if ((xSpeed < 0 && transform.localScale.x > 0) || (xSpeed > 0 && transform.localScale.x < 0))
+        // {
+        //     transform.localScale *= new Vector2(-1, 1);
+        //     dir *= -1;
+        // }
 
         //jump
         grounded = Physics2D.OverlapCircle(feet.position, .5f, ground); 
